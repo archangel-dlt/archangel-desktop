89755559aaeaa405950d82ab25ef997e646e1770
Jez Higgins <jez@jezuk.co.uk>
2018-12-10 11:01:23 +0000

    Connect to local geth client

--- src/main/kotlin/archangeldlt/Start.kt
+++ src/main/kotlin/archangeldlt/Start.kt
@@ -1,7 +1,11 @@
 package archangeldlt
 
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
-
+    val web3j = Web3j.build(HttpService("http://localhost:8545"))
+    println("Connected to Etherum client version: " + web3j.web3ClientVersion().send().web3ClientVersion)
 }
