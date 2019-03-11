90a1e0085a7213d741f1d1622e65b0ecbc12a972
Jez Higgins <jez@jezuk.co.uk>
2019-03-11 09:41:06 +0000

    move to https

--- src/main/kotlin/archangeldlt/ArchangelConfig.kt
+++ src/main/kotlin/archangeldlt/ArchangelConfig.kt
@@ -14,12 +14,12 @@ class ConfigProp(private val key: String, private val defaultValue: String) {
 }
 
 class ArchangelConfig(val config: ConfigProperties) {
-    private val KEY_ENDPOINT = "endpoint"
+    private val KEY_ENDPOINT = "endpoints"
     private val KEY_USERADDRESS = "userAddress"
     private val KEY_WALLETFILE = "walletFile"
     private val KEY_PASSWORD = "password"
 
-    var endpoint: String by ConfigProp(KEY_ENDPOINT, "http://blockchain.surrey.ac.uk/ethereum/")
+    var endpoint: String by ConfigProp(KEY_ENDPOINT, "https://blockchain.surrey.ac.uk/ethereum/")
     var userAddress: String by ConfigProp(KEY_USERADDRESS, "0x0000000000000000000000000000000000000000")
     var walletFile: String by ConfigProp(KEY_WALLETFILE, "")
     var password: String by ConfigProp(KEY_PASSWORD, "")
