63b91bd757314fb00ce939755de40a889ce6d504
Jez Higgins <jez@jezuk.co.uk>
2019-03-13 12:54:52 +0000

    Video upload URL is now https

--- src/main/kotlin/archangeldlt/video/VideoUpload.kt
+++ src/main/kotlin/archangeldlt/video/VideoUpload.kt
@@ -5,7 +5,7 @@ import java.io.File
 import khttp.post as KhttpPost
 
 fun VideoUpload(xipKey: String, fileUuid: String, filePath: String) {
-    val url = "http://blockchain.surrey.ac.uk/videos/upload/${xipKey}/${fileUuid}"
+    val url = "https://blockchain.surrey.ac.uk/videos/upload/${xipKey}/${fileUuid}"
     val response = KhttpPost(
         url = url,
         files = listOf(FileLike(File(filePath)))
